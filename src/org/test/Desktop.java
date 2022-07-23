cc32ceac9e6719bdcce50f090ff1c7a795a3a0c4
RajivranjanV <rajivrockz94@gmail.com>
2022-07-23 19:42:24 +0530

    code

---
+++ src/org/test/Desktop.java
@@ -0,0 +1,24 @@
+package org.test;
+
+public class Desktop implements Hardware,Software {
+
+	@Override
+	public void softwareResource() {
+		System.out.println("softwareResourse use in IT field");
+		
+	}
+
+	@Override
+	public void hardwareResource() {
+		System.out.println("hardware is used in manufacturing field");
+		
+	}
+	public static void main(String[] args) {
+		Desktop d=new Desktop();
+		d.softwareResource();
+		d.hardwareResource();
+		
+		
+	}
+
+}
